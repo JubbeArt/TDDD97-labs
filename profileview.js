4f2f3038009f77947bad9486fbbf013cdcf0d6a2
Schm000L <darklord_dragonslayer@hotmail.com>
2019-01-28 18:23:25 +0100

    Step 6: Home now default, selected tab now marked. (solution still ugly)

--- profileview.js
+++ profileview.js
@@ -1,19 +1,35 @@
-function handleNav(id) {
+function handleNav(button) {
+    homebutton = document.getElementById("homebutton")
+    browsebutton = document.getElementById("browsebutton")
+    accountbutton = document.getElementById("accountbutton")
+
     homeview = document.getElementById("homeview")
     browseview = document.getElementById("browseview")
     accountview = document.getElementById("accountview")
 
-    if(id === 1) {
+    if(button === homebutton) {
             homeview.style.display = 'block'
             browseview.style.display = 'none'
             accountview.style.display = 'none'
-    } else if(id === 2) {
+
+            homebutton.className = 'selected'
+            browsebutton.className = ''
+            accountbutton.className = ''
+    } else if(button === browsebutton) {
             homeview.style.display = 'none'
             browseview.style.display = 'block'
             accountview.style.display = 'none'
+
+            homebutton.className = ''
+            browsebutton.className = 'selected'
+            accountbutton.className = ''
     } else {
             homeview.style.display = 'none'
             browseview.style.display = 'none'
             accountview.style.display = 'block'
+
+            homebutton.className = ''
+            browsebutton.className = ''
+            accountbutton.className = 'selected'
     }
 }
